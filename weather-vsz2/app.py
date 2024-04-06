30ae2feb8cf560b6c0763bac2e5a66d54b2d62b8
devadethanr <125908755+devadethanr@users.noreply.github.com>
2024-04-06 15:15:47 +0530

    final commit

--- weather-vsz2/app.py
+++ weather-vsz2/app.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session
+import json
+import socket
+import requests
 from dotenv import load_dotenv
 from utils.db import get_user, create_user, update_location
 from utils.user import authenticate_user, register_user
@@ -10,6 +13,116 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
+apiKey = 'e5ce83cd46a64d8283a44845240604'
+
+
+def getCondition(conditionText):
+    conditionText = conditionText.lower()
+    if "rain with thunder" in conditionText or "thundery" in conditionText:
+        return "⛈️"
+    elif "rain" in conditionText or "drizzle" in conditionText:
+        return "🌧️"
+    elif "sun" in conditionText or "sunny" in conditionText or "clear" in conditionText:
+        return "☀️"
+    elif (
+        "cloud" in conditionText
+        or "cloudy" in conditionText
+        or "overcast" in conditionText
+    ):
+        return "☁️"
+    elif (
+        "snow" in conditionText
+        or "snowy" in conditionText
+        or "sleet" in conditionText
+        or "blizzard" in conditionText
+    ):
+        return "🌨️"
+    elif "fog" in conditionText or "foggy" in conditionText or "mist" in conditionText:
+        return "🌫️"
+    elif "pellets" in conditionText:
+        return "❄️"
+    else:
+        return conditionText
+
+
+@app.route("/weather/<location>")
+def weather(location):
+    api = requests.get(
+        f"http://api.weatherapi.com/v1/forecast.json?key={apiKey}&q={location}&days=8&aqi=no&alerts=no"
+    )
+    if api.status_code == 400:
+        return redirect("/")
+    data = json.loads(api.text)
+    current = data["current"]
+    location = data["location"]
+    day1 = data["forecast"]["forecastday"][1]
+    day2 = data["forecast"]["forecastday"][2]
+    day3 = data["forecast"]["forecastday"][3]
+    day4 = data["forecast"]["forecastday"][4]
+    day5 = data["forecast"]["forecastday"][5]
+    day6 = data["forecast"]["forecastday"][6]
+    day7 = data["forecast"]["forecastday"][7]
+    return render_template(
+        "weather.html",
+        currentTemp=current["temp_c"],
+        locationName=location["name"],
+        localTime=location["localtime"].split(" ")[1],
+        locationRegion=location["region"],
+        locationCountry=location["country"],
+        currentCondition=getCondition(current["condition"]["text"]),
+        data=[
+            [
+                getCondition(day1["day"]["condition"]["text"]),
+                day1["day"]["avgtemp_c"],
+                day1["day"]["maxtemp_c"],
+                day1["day"]["mintemp_c"],
+                day1["date"].replace("-", "/")[5:],
+            ],
+            [
+                getCondition(day2["day"]["condition"]["text"]),
+                day2["day"]["avgtemp_c"],
+                day2["day"]["maxtemp_c"],
+                day2["day"]["mintemp_c"],
+                day2["date"].replace("-", "/")[5:],
+            ],
+            [
+                getCondition(day3["day"]["condition"]["text"]),
+                day3["day"]["avgtemp_c"],
+                day3["day"]["maxtemp_c"],
+                day3["day"]["mintemp_c"],
+                day3["date"].replace("-", "/")[5:],
+            ],
+            [
+                getCondition(day4["day"]["condition"]["text"]),
+                day4["day"]["avgtemp_c"],
+                day4["day"]["maxtemp_c"],
+                day4["day"]["mintemp_c"],
+                day4["date"].replace("-", "/")[5:],
+            ],
+            [
+                getCondition(day5["day"]["condition"]["text"]),
+                day5["day"]["avgtemp_c"],
+                day5["day"]["maxtemp_c"],
+                day5["day"]["mintemp_c"],
+                day5["date"].replace("-", "/")[5:],
+            ],
+            [
+                getCondition(day6["day"]["condition"]["text"]),
+                day6["day"]["avgtemp_c"],
+                day6["day"]["maxtemp_c"],
+                day6["day"]["mintemp_c"],
+                day6["date"].replace("-", "/")[5:],
+            ],
+            [
+                getCondition(day7["day"]["condition"]["text"]),
+                day7["day"]["avgtemp_c"],
+                day7["day"]["maxtemp_c"],
+                day7["day"]["mintemp_c"],
+                day7["date"].replace("-", "/")[5:],
+            ],
+        ],
+    )
+
 
 #index page routes
 @app.route('/')
@@ -18,6 +131,15 @@ def index():
     Renders the index page.
     """
     return render_template('index.html')
+
+@app.route("/index_home")
+def index_home():
+    """_summary_
+        render page after login
+    Returns:
+        _type_: _description_
+    """
+    return render_template("index_login.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -34,7 +156,7 @@ def login():
         user = authenticate_user(username, password)
         if user:
             session['user_id'] = str(user['_id'])
-            return redirect(url_for('index'))
+            return redirect(url_for('index_home'))
 
         return render_template('login.html', error='Invalid username or password')
 
